0ab9a53942597cd60271576c2cf1f23358a3b9ca
DeEvade <william.redgard@gmail.com>
2024-09-11 22:36:05 +0200

    bootstrapped shadcn and tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import FAQ from './faq/page';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Main page</h1>
+        <Button>Click me</Button>
       </main>
     </div>
   );
