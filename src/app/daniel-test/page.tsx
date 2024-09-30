2dfd016f1efcf855fb2df4766e6f1e3082bae550
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2024-09-30 21:12:06 +0200

    Restructured folder directories and configured redux with Next.js

---
+++ src/app/daniel-test/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react'
+import { useAppSelector } from '@/src/lib/hooks/useAppSelector'
+import { useAppDispatch } from '@/src/lib/hooks/useAppDispatch'
+
+import { fetchCompanyDetails } from '../../lib/features/company/companyAPI'
+
+const ApiTest = () => {
+
+  const dispatch = useAppDispatch();
+
+  const fetchCompanyDetailsHandler = (symbol: string) => {
+    dispatch(fetchCompanyDetails("symbol"))
+  }
+
+  return (
+    <div>
+        <button className='borde w-10 h-10 bg-slate-800' onClick={()=> fetchCompanyDetailsHandler("AAPL")
+        }>Call API Test</button>
+    </div>
+  )
+}
+
+export default ApiTest
---
+++ src/lib/features/company/companyAPI.ts
@@ -0,0 +1,55 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {url, options} from './urls';
+import { Company } from './companyTypes';
+
+export const fetchCompanyDetails = createAsyncThunk(
+    'company/fetchCompanies',
+    async (arg:string, {dispatch, rejectWithValue}) => {
+
+        try{
+            // Id should be fetched from our server and then passed as an argument to the url builder function
+            const companyToSearch:string = arg;
+
+            console.log("Company to search : ", arg);
+
+            // const response = await fetch(url, options);
+            const response = await fetch("https://www.avanza.se/_api/price-chart/stock/573878/compare/1002994?timePeriod=one_month", {
+                "headers": {
+                  "accept": "application/json, text/plain, */*",
+                  "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,sv;q=0.7",
+                  "aza-do-not-touch-session": "true",
+                  "content-type": "application/json;charset=UTF-8",
+                  "priority": "u=1, i",
+                  "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+                  "sec-ch-ua-mobile": "?1",
+                  "sec-ch-ua-platform": "\"Android\"",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "x-securitytoken": "-",
+                  "cookie": "AZACOOKIECONSENT_UX=YES; AZACOOKIECONSENT_ANALYSIS=YES; AZACOOKIECONSENT_MARKETING=YES; _ga=GA1.1.1217373007.1725994723; FPID=FPID2.2.cb4nt4W%2BxpvEuOquX7navjW0uwG1EMOV0jJe1HH4tPo%3D.1725994723; FPLC=F45i8bSaeNCa97pGjRq2Eqa0VfQRda0TcKvrfOa%2FYKDo%2BkcJWBOB%2FYq0BVv5zstDkcGZB3Rsvkn4eOCPPc7QOXZImmVb5TgECGeObu28aa94BzdA%2FWijaB3%2BQCmrPA%3D%3D; AZAPERSISTENCE=0253c8bd2e-1942-40bkjA6JiOFSZZjq9nI2FARQcB4oGZ46Pr_30xYq2tkIT78Z5FQI2th1mDwb9pDCaQH1I; PAGE_ANALYTICS=2e74c1cb-e543-4992-a085-75a890895a26; _ga_W3Z698S33N=GS1.1.1727334519.3.1.1727334535.0.0.70315356",
+                  "Referer": "https://www.avanza.se/aktier/om-aktien.html/573878/soltech-energy-sweden",
+                  "Referrer-Policy": "origin-when-cross-origin"
+                },
+                "body": null,
+                "method": "GET"
+              });
+                
+            const data = await response.json();
+            console.log(data);
+            console.log("Company to search 2 : ", arg);
+            return {
+                id: "1",
+                name: "Apple",
+                revenue: 234,
+                profit: 234,
+                description: "string"} as Company;
+
+        }
+        catch(error: any){
+            console.error('Fetch error:', error);
+            // return rejectWithValue((error as Error).message || 'Failed to fetch company details');
+            return "error";
+        }
+    }
+);
