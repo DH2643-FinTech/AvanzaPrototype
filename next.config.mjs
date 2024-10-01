7010f855605b40e71cfb5e9e7eccd7d127b82330
DeEvade <william.redgard@gmail.com>
2024-10-01 17:35:17 +0200

    action change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Enable React Strict Mode
   swcMinify: true, // Use SWC for minification
   output: 'standalone', // Helps with serverless deployments
   env: {},
