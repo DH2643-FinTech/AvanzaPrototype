f05cddacd91f5673cc9ce32778cf0401ec0c4276
Emil <wahrme.emil@gmail.com>
2024-10-14 12:18:30 +0200

    add (wip): About Us page and view

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react';
+import AboutUsView from '@/src/views/AboutUsView/AboutUsView';
+
+export default function DashboardPage() {
+    return (
+        <main>
+            <AboutUsView />
+        </main>
+    );
+}
