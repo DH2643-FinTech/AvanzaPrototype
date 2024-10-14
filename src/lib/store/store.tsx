204e3cb52187e617fe71380d6976b99969240de6
Emil <wahrme.emil@gmail.com>
2024-10-14 20:07:44 +0200

    add: highlightedStocksReducer and financialReportsReducer to redux store

--- src/lib/store/store.tsx
+++ src/lib/store/store.tsx
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import companyReducer from '@/src/lib/features/company/companySlice';
 import watchlistReducer from '@/src/lib/features/watchlist/watchlistSlice';
 import recentlyVisitedReducer from '@/src/lib/features/recentlyVisited/recentlyVisitedSlice';
+import highlightedStocksReducer from '@/src/lib/features/highlightedStocks/highlightedStocksSlice';
+import financialReportsReducer from '@/src/lib/features/financialReports/financialReportsSlice';
 
 
 export const makeStore = () => {
@@ -10,6 +12,8 @@ export const makeStore = () => {
             company: companyReducer,
             watchlist: watchlistReducer,
             recentlyVisited: recentlyVisitedReducer,
+            highlightedStocks: highlightedStocksReducer,
+            financialReports: financialReportsReducer
         },
     });
 }
