aefd083de48f83cbb203753cba9fd855ea97a3f8
DeEvade <william.redgard@gmail.com>
2024-10-16 13:41:30 +0200

    added debug logs

--- src/lib/api/avanzaCalls.ts
+++ src/lib/api/avanzaCalls.ts
@@ -5,6 +5,8 @@ const avanzaBaseUrl = 'https://www.avanza.se/_api';
 export const avanzaFetchGeneralInfo = async (companyId: string) => {
   const response = await fetch(`${avanzaBaseUrl}/market-guide/stock/${companyId}/details`);
   if (!response.ok) {
+    console.log("Failed to fetch general info");
+    
     throw new Error('Failed to fetch general info');
   }
   return await response.json() as AvanzaCompanyDetailsResponse;
@@ -13,6 +15,7 @@ export const avanzaFetchGeneralInfo = async (companyId: string) => {
 export const avanzaFetchPrice = async (companyId: string) =>{
   const response = await fetch(`${avanzaBaseUrl}/market-guide/stock/${companyId}`);
   if (!response.ok) {
+    console.log("Failed to fetch price info");
     throw new Error('Failed to fetch price info');
   }
   return await response.json() as AvanzaCompanyPriceResponse;
